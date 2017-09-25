7695fa8ff771f46bfbd4b877f7d57c24ddd2fea4
skor <sergey.korotkov@prowidelabs.com>
2017-09-26 00:49:39 +0700

    via hamiltonian cycle

--- src/main/java/StringsRearrangement.java
+++ src/main/java/StringsRearrangement.java
@@ -27,19 +27,42 @@
 public class StringsRearrangement {
 
     boolean stringsRearrangement(String[] inputArray) {
-        boolean[][] graph = getGraph(inputArray, false);
+        boolean[][] graph = getGraph(inputArray);
+        boolean[] visited = new boolean[inputArray.length + 1];
+        visited[0] = true;
+        return searchHamCycle(graph, visited,0, 0, inputArray.length + 1);
+    }
+
+    private boolean searchHamCycle(boolean[][] G, boolean[] visited, int lastFound, int start, int length) {
+        if (G[lastFound][start] && length == 1)
+            return true;
+        for (int i = 0; i < G.length; i++) {
+            if (G[lastFound][i] && !visited[i]) {
+                visited[i] = true;
+                if (searchHamCycle(G, visited, i, start, length - 1))
+                    return true;
+                visited[i] = false;
+            }
+        }
         return false;
     }
 
-    private boolean[][] getGraph(String[] inputArray, boolean addSentinel) {
-        boolean[][] result = new boolean[inputArray.length][inputArray.length];
-        for (int i = 0; i < inputArray.length; i++)
+    private boolean[][] getGraph(String[] inputArray) {
+        boolean[][] result = new boolean[inputArray.length+1][inputArray.length+1];
+        for (int i = 0; i < inputArray.length; i++) {
             for (int j = 0; j < inputArray.length; j++) {
                 if (i != j)
-                    result[i][j] = areClose(inputArray[i], inputArray[j]);
+                    result[i + 1][j + 1] = areClose(inputArray[i], inputArray[j]);
                 else
-                    result[i][j] = false;
+                    result[i + 1][j + 1] = false;
             }
+        }
+        for (int i = 1; i <= inputArray.length; i++) {
+            result[0][i] = true;
+            result[i][0] = true;
+        }
+        result[0][0] = false;
+
         return result;
     }
 
